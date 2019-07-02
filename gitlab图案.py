5e1c98d64848086395f57f2a6d20df9b3ecce494
rookie_robin <52406219+pythonrobin@users.noreply.github.com>
2019-07-02 19:47:35 +0800

    Add files via upload
    循环练习打印图标

---
+++ gitlab图案.py
@@ -0,0 +1,42 @@
+for i in range(1,6):
+    for j in range(0,7-i):
+        print(' ',end='')
+    for k in range(0,2*i-1):
+        print('*',end='')
+    for l in range(0,22-2*i):
+        print(' ', end='')
+    for k in range(0, 2 * i - 1):
+        print('*', end='')
+    print()
+for i in range(6,8):
+    for m in range(0,7-i):
+        print(' ',end='')
+    for n in range(0,2*i-1):
+        print(',',end='')
+    for o in range(0,23-2*i):
+        print('*',end='')
+    for n in range(0, 2 * i - 2):
+        print(',', end='')
+    print()
+for i in range(8,10):
+    for p in range(0,5*i-40):
+        print(' ',end='')
+    for q in range(0,46-4*i):
+        print(',',end='')
+    for o in range(0,23-2*i):
+        print('*',end='')
+    for q in range(0,54-5*i):
+        print(',',end='')
+    print()
+for i in range(10,13):
+    for r in range(0,3*i-21):
+        print(' ',end='')
+    for s in range(0,37-3*i):
+        print(',',end='')
+    for t in range(0,14-i):
+        print('*',end='')
+    for s in range(0,37-3*i):
+        print(',',end='')
+    print()
+
+
